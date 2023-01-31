a448757af2183ae74dba29b8d4c5f6c69db25181
Leomar Moreira <leounbdf@gmail.com>
2023-01-31 16:53:36 -0300

    criando o arquivo html com o js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log(será um projeto teste)
+
+
+
+function calcular() {
+    var n1 = parseInt(document.getElementById('primeiroAtenuante').value, 4);
+    var n2 = parseInt(document.getElementById('segundoAtenuante').value, 4);
+    document.getElementById('resultado').innerHTML = n1 + n2;
+  }
+
+  console.log("resultado")
